46241beddce2a9646154119b07316fb724928870
Alex Kplus <alex@kplus79.ru>
2026-09-10 10:12:05 +0000

    relation-cards-widget: invisible backdrop to close picker on outside click

--- apps/relation-cards-widget/src/front-components/relation-cards.tsx
+++ apps/relation-cards-widget/src/front-components/relation-cards.tsx
@@ -769,6 +769,21 @@ const RelationCards = () => {
 
         {isPickerOpen ? (
           <div
+            onClick={closePicker}
+            style={{
+              position: 'fixed',
+              top: 0,
+              left: 0,
+              right: 0,
+              bottom: 0,
+              zIndex: 9,
+              background: 'transparent',
+            }}
+          />
+        ) : null}
+
+        {isPickerOpen ? (
+          <div
             onClick={(event) => event.stopPropagation()}
             onMouseDown={markInsideInteraction}
             style={{
